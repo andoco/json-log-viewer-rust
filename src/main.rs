1ed2e36d1bdb772500369d014d0f0e0c8dbfee8f
Andrew O'Connor <andrew.oconnor@gmail.com>
2019-08-25 10:25:09 +0100

    Formatting line to string
    Including unit test

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::io::{self, BufRead};
-use serde_json::{Value};
+use serde_json::{Value,json};
 
 #[derive(Debug, Clone)]
 enum LogLineError {
@@ -15,30 +15,41 @@ fn main() {
         let line_str = line_string.as_str();
         let v: Value = serde_json::from_str(line_str).unwrap();
 
-        match print_log_line(v) {
-            Ok(_) => {},
+        match format_log_line(v) {
+            Ok(s) => println!("{}", s),
             Err(LogLineError::Field(v, f)) => println!("Could not print line with json: {}, Problem Field: {:?}", v, f),
         }
     }
 }
 
-fn print_log_line(v: Value) -> Result<(),LogLineError> {
-        let timestamp = match v["timestamp"].as_str() {
-            Some(s) => s,
-            None => return Err(LogLineError::Field(v, String::from("timestamp"))),
-        };
+fn format_log_line(v: Value) -> Result<String,LogLineError> {
+    let timestamp = match v["timestamp"].as_str() {
+        Some(s) => s,
+        None => return Err(LogLineError::Field(v, String::from("timestamp"))),
+    };
 
-        let level = match v["level"].as_str() {
-            Some(s) => s,
-            None => return Err(LogLineError::Field(v, String::from("level"))),
-        };
+    let level = match v["level"].as_str() {
+        Some(s) => s,
+        None => return Err(LogLineError::Field(v, String::from("level"))),
+    };
 
-        let message = match v["message"].as_str() {
-            Some(s) => s,
-            None => return Err(LogLineError::Field(v, String::from("message"))),
-        };
+    let message = match v["message"].as_str() {
+        Some(s) => s,
+        None => return Err(LogLineError::Field(v, String::from("message"))),
+    };
 
-        println!("{timestamp} [{level}] {message}", timestamp=timestamp, level=level, message=message);
+    let line = format!("{timestamp} [{level}] {message}", timestamp=timestamp, level=level, message=message);
 
-        Ok(())
+    Ok(String::from(line))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_format_log_line() {
+        let v = json!({"timestamp": "2019-08-25T09:00:00", "level": "info", "message": "a test message"});
+        assert_eq!("2019-08-25T09:00:00 [info] a test message", format_log_line(v).unwrap())
+    }
 }
